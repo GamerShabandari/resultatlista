2491b56e2fc1512ab7e9f7a8e2749bdba4a459fc
GamerSh <93838417+GamerShabandari@users.noreply.github.com>
2021-11-26 08:54:38 +0100

    fixat lista med spelare och poäng

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("hejehej");
+let spelare = ["Gamer", "Maldini" , "Weah", "Zlatan", "Kaka", "Seedorf"];
+
+console.log(spelare);
+
+let spelarnasPoang = [0, 0, 0, 0, 0, 0];
+
+console.log(spelarnasPoang);
+
+let minContainer = document.getElementById("container");
+
+
+for (let i = 0; i < spelare.length; i++){
+
+    minContainer.insertAdjacentHTML("beforeend","<li>"+spelare[i]+"</li>");
+    minContainer.insertAdjacentHTML("beforeend", "<li>" + "poäng: " +spelarnasPoang[i]+"</li>");
+    minContainer.insertAdjacentHTML("beforeend","<button> - </button>");
+    minContainer.insertAdjacentHTML("beforeend","<button> + </button>");
+
+};
